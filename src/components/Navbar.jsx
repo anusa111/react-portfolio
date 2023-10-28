a1b2f6b61157822c83dbc5db322c4fa7f36fc829
anusa111 <anusakhadka16@gmail.com>
2023-10-28 20:16:33 +0545

    changed few

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,25 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 function BasicExample() {
   return (
-    <Navbar  expand="lg"  className="navbar">
+    <Navbar expand="lg" className="navbar">
       <Container className="nav-list" fluid>
-        <Navbar.Brand>Anusa</Navbar.Brand>
+        <Navbar.Brand>Anusha</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className="nav-link" >
+            <Link to="/" className="nav-link">
               Home
             </Link>
-            <Link to="/skills" className="nav-link" >
+            <Link to="/skills" className="nav-link">
               Skills
             </Link>
-            <Link to="/projects" className="nav-link" >
+            <Link to="/projects" className="nav-link">
               Projects
             </Link>
-            <Link to="/contacts" className="nav-link" >
+            <Link to="/contacts" className="nav-link">
               Contact
             </Link>
-            <Link to="/about" className="nav-link" >
+            <Link to="/about" className="nav-link">
               About Me
             </Link>
           </Nav>
